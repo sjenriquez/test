27108ab907cf2c0455a2c5bc296bb175c1409e6b
Scott Enriquez <sjenriquez@gmail.com>
2017-06-27 16:40:47 -0400

    make it a question

--- hi.py
+++ hi.py
@@ -5,7 +5,7 @@ def b():
    print("hello")
 
 def c():
-  print("why not have a convo with myself")
+  print("why not have a convo with myself?")
 
 def d():
   print("yet another useless function")
